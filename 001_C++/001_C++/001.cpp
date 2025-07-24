207a0465fff048e0a6f7ba786ed347bcc091850f
NikitaIskra <167676193+NikitaIskra@users.noreply.github.com>
2025-07-24 12:32:51 +0300

    Add files via upload

---
+++ 001_C++/001_C++/001.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include  "string.h"
+
+using namespace std;
+//using std::endl;
+
+int main()
+{
+    //string name;
+    //string surname;
+    //int a = 0;
+    //int b = 10;
+    //int c{ 5 };
+    //unsigned short d = 2355;
+    //float f = 12.3;
+    //double do1 = 0.001;
+    //char ch = 'C';
+    //bool isSigned = false;
+    ////char <= short <= int <= long int <= long long
+    ////cout << "Hello World!" << endl;
+    ////cin >> a >> b;
+    ////cout << "Garbage_1: " << a << endl;
+    ////cout << "Garbage_2: " << b << endl;
+    //////cout << "Garbage_3: " << c << endl;
+    ////cout <<"Bool: " << isSigned << endl;
+    //cout << "Enter your name: ";
+    //cin >> name;
+    //cout << "Enter your surname: ";
+    //cin >> surname;
+    //cout << "Your name: " << name << endl;
+    //cout << "Your surname: " << surname << endl;
+    int a, b, c;
+    cin >> a >> b >> c;
+    int s = 0;
+    s = a + b + c;
+    cout << "Enter sum of numbers: " << s << endl;
+    return 0;
+}
