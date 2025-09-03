797788c03a9472b7adea555dfb428f8f08b3c6f0
NikitaIskra <167676193+NikitaIskra@users.noreply.github.com>
2025-09-03 17:14:41 +0300

    Add files via upload

---
+++ Dz_007/Dz_007.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+//1
+//void mas1(int a[][3], int n) {
+//    int min = a[0][0];
+//    int max = a[0][0];
+//    for (int i = 0; i < n; i++) {
+//        sort(a[i], a[i] + n);
+//    }
+//    for (int i = 0; i < n; i++) {
+//        if (a[i][i] > max) max = a[i][i];
+//        if (a[i][i] < min) min = a[i][i];
+//    }
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n; j++) cout << a[i][j] << "\t";
+//        cout << "\n\n";
+//    }
+//    cout << "Min: " << min << "Max: " << max << endl;
+//}
+//void mas2(double b[][3], int n) {
+//    double min = b[0][0];
+//    double max = b[0][0];
+//    for (int i = 0; i < n; i++) {
+//        sort(b[i], b[i] + n);
+//}
+//    for (int i = 0; i < n; i++) {
+//        if (b[i][i] > max) max = b[i][i];
+//        if (b[i][i] < min) min = b[i][i];
+//    }
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n; j++) cout << b[i][j] << "\t";
+//        cout << "\n\n";
+//    }
+//    cout << "Min: " << min << "Max: " << max << endl;
+//}
+//void mas3(char c[][3], int n) {
+//    char min = c[0][0];
+//    char max = c[0][0];
+//    for (int i = 0; i < n; i++) {
+//        sort(c[i], c[i] + n);
+//    }
+//    for (int i = 0; i < n; i++) {
+//        if (c[i][i] > max) max = c[i][i];
+//        if (c[i][i] < min) min = c[i][i];
+//    }
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n; j++) cout << c[i][j] << "\t";
+//        cout << "\n\n";
+//    }
+//    cout << "Min: " << min << "Max: " << max << endl;
+//}
+
+//int n = 3;
+//int a[3][3] = { {5,  2,  8}, { 1,  4,  9 }, { 3,  7, 6 } };
+//double b[3][3] = { {1.1, 2.2, 3.3},{4.4, 5.5, 6.6},{7.7, 8.8, 9.9} };
+//char c[3][3] = { {'A','B','C'},{'D','E','F'},{'G','H','I'} };
+//mas1(a, n);
+//mas2(b, n);
+//mas3(c, n);
+//2
+//int dil(int a, int b) {
+//	if (b == 0) {
+//		return a;
+//	return dil(b, a % b);
+//	}
+//}
+//int a = 2;
+//int b = 5;
+////cout << dil(a, b) << endl;
+
+int main()
+{
+
+}
+
