208ceb07a5491a86e864ce62509284925a1ccdef
NikitaIskra <167676193+NikitaIskra@users.noreply.github.com>
2025-09-27 16:11:08 +0300

    Add files via upload

---
+++ 020/020.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+#define LOG(msg) \
+			cout << "[" << __DATE__ << " " << __TIME__ << " " << __FILE__ "]" << msg << endl;
+
+int main()
+{
+	LOG("I start my program");
+	LOG("I finished my program");
+}
+
